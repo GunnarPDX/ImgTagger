de99806ee4bc1697c96d004343ae705ceeb5e40d
Gunnar R <gunnarrosenberg@gmail.com>
2019-10-08 16:40:49 -0700

    update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,7 @@ class ProductsController < ApplicationController
 
   def index
     # @products = Product.all
-    @products = Product.all # .search(params[:search]).paginate(page: params[:page], per_page: 3)
+    @products = Product.all.paginate(page: params[:page], per_page: 6) # .search(params[:search]).paginate(page: params[:page], per_page: 3)
   end
 
   def show
